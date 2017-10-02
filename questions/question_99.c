9198e00268148e9ae7844c72d31320ba820a56ed
Luiz Santos <97luizsantos@gmail.com>
2017-10-02 18:26:10 -0300

    Update question_99.c

--- questions/question_99.c
+++ questions/question_99.c
@@ -5,7 +5,71 @@
 		cada linha ou coluna é permutação dos N primeiros números inteiros.
 
 */    
-
 main(){
 	
 }
+
+
+/* STRUCT */
+
+/*
+struct agenda{
+	
+	char nome[100];
+	char cpf[50];
+	char telefone[50]; 
+	int idade;
+	char end[100];
+		
+};
+
+struct agenda vet[2];
+int x, achou;
+char cod_cpf[50];
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	for(x=0; x<2; x++){
+		
+		printf("Informe o nome:\n");
+		gets(vet[x].nome);	
+			
+		printf("Informe o CPF:\n");
+		gets(vet[x].cpf);		
+		
+		printf("Informe o telefone:\n");
+		gets(vet[x].telefone);
+		
+		printf("Informe a Idade:\n");
+		scanf("%d", &vet[x].idade);
+		fflush(stdin);
+		
+		printf("Informe o endereco:\n");
+		gets(vet[x].end);
+		
+		printf("\n\n");
+	}
+		
+		
+		
+	achou=0;
+	printf("Informe um CPF para buscar a pessoa:\n");
+	gets(cod_cpf);
+	
+	for(x=0; x<2; x++){
+		
+		if(strcmp(cod_cpf, vet[x].cpf)==0){
+			
+			printf("%sNome:\n", vet[x].nome);
+			printf("%sCPF:\n", vet[x].cpf);
+			printf("%sTelefone:\n", vet[x].telefone);
+			printf("%dIdade\n", vet[x].idade);
+			printf("%sEndereco\n", vet[x].end);	
+		}
+		
+	}	
+	
+	return 0;
+}*/
